9a8785056a24e5ff4c4f17159506a530cd17aea1
Thein Htut <theinhtut.dec@gmail.com>
2019-12-13 23:12:32 +0800

    Redesigned Service Request Form

--- src/components/ServiceRequestForm.js
+++ src/components/ServiceRequestForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Button, TextField } from '@material-ui/core'
+import { Button, TextField, Typography } from '@material-ui/core'
+import formImg from '../images/form-submit.svg'
+import '../styles/service-request-form.css'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,89 +19,102 @@ const useStyles = makeStyles((theme) => ({
 export default function ServiceRequestForm() {
   const classes = useStyles()
   return (
-    <div className={classes.root}>
-      <div>
-        <TextField
-          id='filled-full-width'
-          label='Unit Number'
-          style={{ margin: 8 }}
-          fullWidth
-          margin='normal'
-          variant='filled'
-        />
-        <TextField
-          id='filled-full-width'
-          label='Owner Name'
-          style={{ margin: 8 }}
-          fullWidth
-          margin='normal'
-          variant='filled'
-        />
-        <TextField
-          id='filled-full-width'
-          label='Contact Number'
-          style={{ margin: 8 }}
-          fullWidth
-          margin='normal'
-          variant='filled'
-        />
-        <TextField
-          id='filled-full-width'
-          label='Additonal Notes'
-          style={{ margin: 8 }}
-          fullWidth
-          margin='normal'
-          variant='filled'
-          helperText='Maximum 300 charaters'
-        />
-        <Button onClick={() => console.log('You have submit the request')} variant='contained' color='primary'>Submit Request</Button>
+    <section>
+      <Typography variant='h4'>Service Request Form</Typography>
+      <div className={classes.root}>
+        <div className='request-form__input-wrapper'>
+          <TextField
+            id='filled-full-width'
+            label='Unit Number'
+            style={{ margin: 8 }}
+            fullWidth
+            margin='normal'
+            variant='filled'
+          />
+          <TextField
+            id='filled-full-width'
+            label='Owner Name'
+            style={{ margin: 8 }}
+            fullWidth
+            margin='normal'
+            variant='filled'
+          />
+          <TextField
+            id='filled-full-width'
+            label='Contact Number'
+            style={{ margin: 8 }}
+            fullWidth
+            margin='normal'
+            variant='filled'
+          />
+          <TextField
+            id='filled-full-width'
+            label='Additonal Notes'
+            style={{ margin: 8 }}
+            fullWidth
+            margin='normal'
+            variant='filled'
+            helperText='Maximum 300 charaters'
+          />
+          <Button
+            className='request-form__btn'
+            onClick={() => console.log('You have submit the request')}
+            variant='contained'
+            color='primary'
+          >
+            Submit Request
+          </Button>
+        </div>
+        <div className='request-form'>
+          <img className='request-form__img' src={formImg} alt='' />
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
-                      // export default class ServiceRequestForm extends Component {
-                      //   render() {
-                      //       const classes = useStyles()
-                      //     return (
-                      //       <div className={classes.root}>
-                      //         <div>
-                      //           <TextField
-                      //             id='filled-full-width'
-                      //             label='Unit Number'
-                      //             style={{ margin: 8 }}
-                      //             fullWidth
-                      //             margin='normal'
-                      //             variant='filled'
-                      //           />
-                      //           <TextField
-                      //             id='filled-full-width'
-                      //             label='Owner Name'
-                      //             style={{ margin: 8 }}
-                      //             fullWidth
-                      //             margin='normal'
-                      //             variant='filled'
-                      //           />
-                      //           <TextField
-                      //             id='filled-full-width'
-                      //             label='Contact Number'
-                      //             style={{ margin: 8 }}
-                      //             fullWidth
-                      //             margin='normal'
-                      //             variant='filled'
-                      //           />
-                      //           <TextField
-                      //             id='filled-full-width'
-                      //             label='Additonal Notes'
-                      //             style={{ margin: 8 }}
-                      //             fullWidth
-                      //             margin='normal'
-                      //             variant='filled'
-                      //             helperText='Maximum 300 charaters'
-                      //           />
-                      //           <Button variant='contained' color='primary'>Submit Request</Button>
-                      //         </div>
-                      //       </div>
-                      //     )
-                      //   }
-                      // }
+// export default class ServiceRequestForm extends Component {
+//   render() {
+//       const classes = useStyles()
+//     return (
+//       <div className={classes.root}>
+//         <div>
+//           <TextField
+//             id='filled-full-width'
+//             label='Unit Number'
+//             style={{ margin: 8 }}
+//             fullWidth
+//             margin='normal'
+//             variant='filled'
+//           />
+//           <TextField
+//             id='filled-full-width'
+//             label='Owner Name'
+//             style={{ margin: 8 }}
+//             fullWidth
+//             margin='normal'
+//             variant='filled'
+//           />
+//           <TextField
+//             id='filled-full-width'
+//             label='Contact Number'
+//             style={{ margin: 8 }}
+//             fullWidth
+//             margin='normal'
+//             variant='filled'
+//           />
+//           <TextField
+//             id='filled-full-width'
+//             label='Additonal Notes'
+//             style={{ margin: 8 }}
+//             fullWidth
+//             margin='normal'
+//             variant='filled'
+//             helperText='Maximum 300 charaters'
+//           />
+//           <Button variant='contained' color='primary'>Submit Request</Button>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
